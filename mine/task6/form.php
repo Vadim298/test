123feaad0beec1e4d1de859ad3f9da8f5f6ccd15
vadym <vadym298@gmail.com>
2017-06-27 09:38:26 +0000

    remove

--- mine/task6/form.php
+++ mine/task6/form.php
@@ -9,11 +9,10 @@
 <html>
 <body>
 <form action="index.php" method="post">
-    <label>Login    <input type="text"     name="login">    </label><br>
+    <label>Login <input type="text" name="login"> </label><br>
     <label>Password <input type="password" name="password"> </label><br>
-    <!---<label>Name     <input type="text"     name="name">     </label><br>
-    <label>Address  <input type="text"     name="address">  </label><br>--->
-    <label>         <input type="submit"   value="enter">   </label><br>
+    <label> <input type="submit" value="enter"> </label><br>
+    <a href="registration_form.php?registration">Регистрация</a>
 </form>
 </body>
 </html>
---
+++ mine/task6/registration_form.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vadym
+ * Date: 27.06.17
+ * Time: 8:47
+ */
+?>
+<html>
+<form action="registration_form.php" method="post">
+    <label>Придумайте логин<input type="text" name="login"> </label><br>
+    <label>Придумайте пароль <input type="password" name="password"> </label><br>
+    <label>Ваше имя <input type="text" name="username"> </label><br>
+    <label>Ваше место пребывания <input type="text" name="location"> </label><br>
+    <label><input type="submit" value="отправить данные"></label><br>
+    <label>Уже есть аккаунт ----><a href="index.php?enter">Войти</a></label><br>
+</form>
+</html>
+<?php
+    if(isset($_POST['login'], $_POST['password'], $_POST['username'], $_POST['location'])){
+        $login = $_POST['login'];
+        $password = $_POST['password'];
+        $username = $_POST['username'];
+        $location = $_POST['location'];
+        file_put_contents('db.txt', "$login | $password | $username | $location\n", FILE_APPEND);
+    }
+
+?>
+
