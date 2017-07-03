3502ffab284f07d88363ec3b1f8d8b45c402c430
vadym <vadym298@gmail.com>
2017-07-03 14:18:44 +0000

    Guestbook

--- php_osnovy/lvl2/sql/sql.php
+++ php_osnovy/lvl2/sql/sql.php
@@ -5,8 +5,23 @@
  * Date: 30.06.17
  * Time: 14:44
  */
-mysql_connect("localhost",
+$db = mysqli_connect("localhost",
     "root",
     "killallhumans");
 
-mysql_close();
+mysqli_select_db($db, "test_db")
+    or die (mysqli_error($db));
+mysqli_query($db, "SET NAMES 'utf8'");
+$sql = "SELECT * FROM teachers";
+$result = mysqli_query($db, "$sql")
+    or die (mysqli_error($db));
+mysqli_close($db);
+
+while($row = mysqli_fetch_assoc($result)){
+    echo $row["name"]."<br>";
+}
+
+/*echo '<pre>';
+print_r ($row);
+echo '</pre>';*/
+
